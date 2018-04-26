606d641111db3d1a060385df195e4bd5ebb00ea0
lichen545 <lichen545@gmail.com>
2018-04-26 10:41:01 -0500

    added labels for Space/Scene Selection

--- src/components/control-automation/DeviceList.js
+++ src/components/control-automation/DeviceList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { ListGroup, ListGroupItem, Button } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Button, Label } from 'react-bootstrap';
 
 import Device from './Device';
 
@@ -8,39 +8,52 @@ class DeviceList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        // devices: props.devices
+      // devices: props.devices
     };
   }
 
   render() {
-    return ( 
-      <div className="deviceList" style={{height: "100%"}}>
+    return (
+      <div className="deviceList" style={{ height: "100%" }}>
         <div className="listHeader">
-            {
-              this.props.isScene ? (
-                  !this.props.devices.find((device) => device.status !== device.deviceSceneStatus) ?
-                    (<Button bsStyle="success" className="activateScene" disabled>Active</Button>) :
-                    (<Button bsStyle="success" className="activateScene" onClick={() => {this.props.onSceneActivation(this.props.devices, this.props.componentName);}}>Activate Scene</Button>)
-              ) : (
-                  ""
+          {
+            this.props.isScene ? (
+              !this.props.devices.find((device) => device.status !== device.deviceSceneStatus) ?
+                (<Button bsStyle="success" className="activateScene" disabled>Active</Button>) :
+                (<Button bsStyle="success" className="activateScene" onClick={() => { this.props.onSceneActivation(this.props.devices, this.props.componentName); }}>Activate Scene</Button>)
+            ) : (
+                ""
               )
-            }
-          <div className="listTitle">Devices ({this.props.componentName})</div>
+          }
+          <div className="listTitle">
+            {this.props.componentName === 'All' ? (
+              <React.Fragment>Devices: All</React.Fragment>
+            ) : (
+              <React.Fragment>
+              Devices&nbsp;
+                <Label bsStyle="info">
+                  {this.props.componentName}&nbsp;
+                  <a><i class="remove glyphicon glyphicon-remove-circle glyphicon-white"></i></a>
+                </Label>
+              </React.Fragment>
+            )}
+          </div>
+
         </div>
         <div className="containerDiv">
-            <div className="info">Current Status</div>
-            {
-              this.props.isScene ? (
-                <div className="sceneInfo">Status on Activation</div>
-              ) : ("")
-            }
+          <div className="info">Current Status</div>
+          {
+            this.props.isScene ? (
+              <div className="sceneInfo">Status on Activation</div>
+            ) : ("")
+          }
         </div>
-        <div style={{overflowY: "scroll", maxHeight: "80%"}}>        
+        <div style={{ overflowY: "scroll", maxHeight: "80%" }}>
           <ListGroup>
             {
               this.props.devices.map((d, i) => (
                 <ListGroupItem key={i}>
-                  <Device info={d.info} status={d.status} deviceSceneStatus={d.deviceSceneStatus} isScene={this.props.isScene} onStatusChange={this.props.onStatusChange}/>
+                  <Device info={d.info} status={d.status} deviceSceneStatus={d.deviceSceneStatus} isScene={this.props.isScene} onStatusChange={this.props.onStatusChange} />
                 </ListGroupItem>
               ))
             }
@@ -50,5 +63,5 @@ class DeviceList extends Component {
     );
   }
 }
- 
+
 export default DeviceList;
